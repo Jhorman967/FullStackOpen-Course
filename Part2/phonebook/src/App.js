1abcb11747464fdb5ca1726659f1f0ecf88ef862
Jhorman967 <jhorman967@gmail.com>
2023-04-04 22:10:22 -0500

    add exercise 2.6 2.7 2.8 2.9 2.10

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import PersonForm from './components/PersonalForm';
+import Number from './components/Number';
+import Searching from './components/Search';
+
 
 function App() {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+  ]) 
+
+  const [ newName, setNewName ] = useState('')
+  const [phnumber, setPhnumber] = useState('')
+  const [search, setSearch] = useState('')
+
+
+  const addName = (event)=>{
+    const test = persons.find(names =>names.name === newName)
+    if (test === undefined) {
+      event.preventDefault();//remember help for no reload page
+    const personObject={
+      name: newName,
+      phone: phnumber,
+      id: persons.length +1, 
+    }
+    setPersons(persons.concat(personObject))
+    setNewName("")  
+    setPhnumber("")
+    persons.map(person=>console.log("this is a ", person))
+    } else {
+      alert(`Upss sorry but ${newName} is in our system`) 
+      setNewName("") 
+      setPhnumber("")
+    }
+  }
+
+  const readName = (event)=>(setNewName(event.target.value))
+
+  const readPhone= (event)=>(setPhnumber(event.target.value))
+
+  const readSearch= (event)=>(setSearch(event.target.value))
+
+  const filterPerson =  persons.filter((per)=> per.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
-    <div className="App">
- 
+    <div>
+      <h1>Phonebook</h1>
+      <Searching filter={filterPerson} search={search} readsearch={readSearch}/>
+      <PersonForm addname={addName} newname={newName} readname={readName} phnumber={phnumber} readphone={readPhone} />
+      <h2>Numbers</h2>
+      <Number key={persons} numbers={persons}/>
     </div>
-  );
+  )
 }
 
 export default App;

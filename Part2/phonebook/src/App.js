8af33b2667ebadad43ee5f166832404f7f2462c2
Jhorman967 <jhorman967@gmail.com>
2023-04-06 20:05:46 -0500

    all exercise done part 2

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,25 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import PersonForm from './components/PersonalForm';
 import Number from './components/Number';
 import Searching from './components/Search';
+import PersonsService from './services/PersonsService';
+import MenGood from './components/NotificationGood';
+import MenError from './components/NotificationError';
+
+
+
+
+
 
 
 function App() {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
 
   const [ newName, setNewName ] = useState('')
   const [phnumber, setPhnumber] = useState('')
   const [search, setSearch] = useState('')
+  const [mensaje, setMensaje] = useState([null])
+
+ 
+
+
+  useEffect(()=>{
+    PersonsService
+    .getAll()
+    .then(response=>{
+      setPersons(response)
+    }).catch(error=>{
+      console.log('Ups can read the data', error)
+    })
+  },[])
+
+
+
+
+
+
 
 
   const addName = (event)=>{
     const test = persons.find(names =>names.name === newName)
+    
     if (test === undefined) {
       event.preventDefault();//remember help for no reload page
     const personObject={
@@ -27,16 +51,66 @@ function App() {
       phone: phnumber,
       id: persons.length +1, 
     }
-    setPersons(persons.concat(personObject))
-    setNewName("")  
-    setPhnumber("")
+    PersonsService
+    .create(personObject)
+    .then(response =>{
+      setPersons(persons.concat(response))
+      setMensaje(mensaje.unshift("exito"))
+      setMensaje(mensaje.concat(`Very good ${newName} was save successfully`))
+      setTimeout(() => {
+        setMensaje([null])
+      }, 3000)
+      setNewName('')
+      setPhnumber('')
+    }).catch(error => {
+      setMensaje(mensaje.unshift("error"))
+      setMensaje(mensaje.concat(`ups we can't save  ${newName} Erro: ${error.message}}`))
+      setTimeout(() => {
+        setMensaje([null])
+      }, 3000)
+    }) 
+
     persons.map(person=>console.log("this is a ", person))
-    } else {
-      alert(`Upss sorry but ${newName} is in our system`) 
+    } else if (window.confirm(`the ${test.name} is in our system, you want to change the number`)) {
+      const personObject={
+        name: newName,
+        phone: phnumber
+      }
+
+      PersonsService
+      .update(test.id,personObject).then(response => {console.log("ey",response)})
+      .catch(error =>{
+        setMensaje(mensaje.unshift("error"))
+      setMensaje(mensaje.concat(`ups we can't save  ${newName} Erro: ${error.message}}`))
+      setTimeout(() => {
+        setMensaje([null])
+      }, 3000)
+      })
       setNewName("") 
       setPhnumber("")
+      
     }
   }
+
+  
+
+
+
+const toggleDeleteOf= (id)=>{
+  if(window.confirm("Are you sure you want to delete")){
+  PersonsService 
+  .delet(id)
+  .then(setNewName("") )
+  .catch(error => {
+    setMensaje(mensaje.unshift("error"))
+    setMensaje(mensaje.concat(`ups we can't save  ${newName} Erro: ${error.message}}`))
+      setTimeout(() => {
+        setMensaje([null])
+      }, 3000)
+    })
+  }
+
+}
 
   const readName = (event)=>(setNewName(event.target.value))
 
@@ -46,14 +120,19 @@ function App() {
 
   const filterPerson =  persons.filter((per)=> per.name.toLowerCase().includes(search.toLowerCase()))
 
-  return (
+
+ 
+  return ( 
     <div>
       <h1>Phonebook</h1>
+      <MenError mensaje={mensaje}/>
       <Searching filter={filterPerson} search={search} readsearch={readSearch}/>
       <PersonForm addname={addName} newname={newName} readname={readName} phnumber={phnumber} readphone={readPhone} />
       <h2>Numbers</h2>
-      <Number key={persons} numbers={persons}/>
+
+      {persons.map(person => <Number key={person.id} name={person.name} phone ={person.phone} toggleDelete={()=>toggleDeleteOf(person.id)}  />)}
     </div>
+    
   )
 }
 
--- Part2/phonebook/src/components/Number.js
+++ Part2/phonebook/src/components/Number.js
@@ -1,13 +1,24 @@
 import React from "react"
+import PersonsService from "../services/PersonsService"
 
-const Number = ({numbers}) =>(
 
-    numbers.map(number => 
-    <div key={number.id}>
-      <h5>{number.name} {number.phone}</h5>
+
+
+const Number = (props) =>{
+
+
+
+    return(
+   
+    <div key={props.name}>
+      <form onSubmit={props.toggleDelete}>  
+      <h5>{props.name} {props.phone} 
+      <button  type='submit'>delete</button>
+      </h5>
+      </form>
     </div>
 
     )
-)
+}
 
 export default Number
